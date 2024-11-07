8c9b98ed0e12f17f19a421c486324bf3392978a6
Grant McKenna <grant.mckenna@madetech.com>
2024-11-07 15:23:26 +0000

    Amend homepage to include event listeners

--- App.tsx
+++ App.tsx
@@ -65,6 +65,10 @@ function App(): React.JSX.Element {
   };
 
   Tts.setIgnoreSilentSwitch('ignore');
+  Tts.addEventListener('tts-start', (event) => console.log('start:', event));
+  Tts.addEventListener('tts-progress', (event) => console.log('in progress:', event));
+  Tts.addEventListener('tts-finish', (event) => console.log('finish:', event));
+  Tts.addEventListener('tts-cancel', (event) => console.log('cancel:', event));
 
   const handleOnPress = () => {
     Tts.speak(text, {
